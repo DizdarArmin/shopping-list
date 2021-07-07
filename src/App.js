d982fc981ea6f494bf6bb05bf44c3497df40c13a
DizdarArmin <armin.dizdar@gmail.com>
2021-07-07 13:25:45 +0200

    stash

--- src/App.js
+++ src/App.js
@@ -12,9 +12,10 @@ function App() {
   const [byPrice, setByPrice] = useState(false);
   const [byName, setByName] = useState(false);
 
+  const [completedList, setCompletedList] = useState([])
   const [list, setList] = useState([
       {key:Math.random(), isFinished: true ,name: "C", price: 500},
-      {key:Math.random(), isFinished: true, name: "A", price: 2500},
+      {key:Math.random(), isFinished: false, name: "A", price: 2500},
       {key:Math.random(), isFinished: false, name: "B", price: 22500},
       {key:Math.random(), isFinished: false ,name: "G", price: 300},
       {key:Math.random(), isFinished: false, name: "H", price: 12},
@@ -62,7 +63,6 @@ const sortByPrice = (e) => {
 
 const notCompleted = list.filter(item => item.isFinished === false).map((item) =>(
   <Task key={item.key} 
-        changed={setToCompleted}
         state={item.isFinished} 
         name={item.name} 
         price={item.price}></Task>
@@ -75,11 +75,11 @@ const completed = list.filter(item => item.isFinished === true).map((item) =>(
         price={item.price}></Task>
 ))
 
-function setToCompleted(){
+function setToCompleted(item){
   setList({
     list:{
       ...list,
-  
+      
       item: true
     }
   })
@@ -111,7 +111,6 @@ function change(e){
 
         <button className="add btn btn-secondary">Add item</button>
         <a className="form-check" onClick={change} href="/">{toggleText}</a>
-        
       </header>
     </div>
   );
